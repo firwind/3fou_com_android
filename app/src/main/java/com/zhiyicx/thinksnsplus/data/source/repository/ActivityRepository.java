717767cdf96fc2f89238c20536fa4b581a22d9aa
zhanglian0715 <1428907383@qq.com>
2018-09-07 10:14:54 +0800

    活动包名及类名

--- app/src/main/java/com/zhiyicx/thinksnsplus/data/source/repository/ActivityRepository.java
+++ app/src/main/java/com/zhiyicx/thinksnsplus/data/source/repository/ActivityRepository.java
@@ -1,7 +1,7 @@
 package com.zhiyicx.thinksnsplus.data.source.repository;
 /*
  * 文件名：
- * 创建者：Administrator
+ * 创建者：zl
  * 时  间：2018/9/7 0007
  * 描  述：
  * 版  权：九曲互动
---
+++ app/src/main/java/com/zhiyicx/thinksnsplus/modules/act/act_center/ActCenterActivity.java
@@ -0,0 +1,12 @@
+package com.zhiyicx.thinksnsplus.modules.act.act_center;
+/*
+ * 文件名：
+ * 创建者：Administrator
+ * 时  间：2018/9/7 0007
+ * 描  述：
+ * 版  权：九曲互动
+ * 
+ */
+
+public class ActCenterActivity {
+}
